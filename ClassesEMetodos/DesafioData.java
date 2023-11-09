76d40706a8a0c1f997994a0dc85bbd54d8f8dc84
Nicolas Formigoni <138242644+NicolasFormigoni@users.noreply.github.com>
2023-11-08 21:03:48 -0300

    Add files via upload

---
+++ ClassesEMetodos/DesafioData.java
@@ -0,0 +1,7 @@
+package ClassesEMetodos;
+//PRIMEIRA PARTE DO PROGRAMA
+public class DesafioData {
+    int dia;
+    int mes;
+    int ano;
+}
---
+++ ClassesEMetodos/DesafioDataTeste.java
@@ -0,0 +1,18 @@
+package ClassesEMetodos;
+
+import java.util.Scanner;
+//SEGUNDA PARTE DO PROGRAMA
+public class DesafioDataTeste {
+    public static void main(String[] args) {
+        Scanner tc = new Scanner(System.in);
+        DesafioData chamar = new DesafioData();
+        System.out.println("Informe o dia do seu nascimento: ");
+        chamar.dia = tc.nextInt();
+        System.out.println("Informe o mês do seu nascimento: ");
+        chamar.mes = tc.nextInt();
+        System.out.println("Informe o ano do seu nascimento: ");
+        chamar.ano = tc.nextInt();
+        System.out.println("Vc nasceu nessa data: " + chamar.dia + "/" + chamar.mes + "/" + chamar.ano);
+        tc.close();
+    }
+}
